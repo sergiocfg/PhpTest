445ec05c4c6732fb161e07ba85a3179b06d6bc96
sergiocfg <soboudy@gmail.com>
2017-02-27 17:13:46 -0500

    db11

---
+++ db/delete.php
@@ -0,0 +1,39 @@
+<?php
+require "select.php";
+print_r($_POST);
+$id = $_POST["id"];
+
+$host = $_POST["host"];
+$user = $_POST["user"];
+$pass = $_POST["pass"];
+$db   = $_POST["db"];
+$mytable = $_POST["mytable"];/**/
+
+
+/*
+$records = array("id"=>$_POST["id"],
+    "name" => $_POST["name"],
+    "lastName" => $_POST["lastName"],
+    "score" => $_POST["score"]);
+print_r($records);*/
+echo "<br/>";
+
+
+$mysql = new mysqli($host, $user, $pass, $db);
+
+if ($mysql->connect_error) {
+    die("Error: (". $mysql->connect_errno.').'.$mysql->connect_error);
+}else {echo "Good connection...";}//else {throw new Error("Connection error...");}
+echo "<br/>";
+
+$sql = "delete from student where id = '$id'";
+
+if($mysql->query($sql) == true)
+{
+    echo "..Done..<br/>";
+}else {
+    echo "Error: " . $sql;
+}
+$mysql->close();
+
+mysqlSelect($host,$user,$pass,$db,$mytable);
